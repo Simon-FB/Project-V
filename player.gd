6208906ee2c188825d3e6e9958727498bcc4aa80
Simon FALCONNET BANEYX <simon.falconnet-baneyx@etu.u-pec.fr>
2026-02-17 15:18:53 +0100

    nouvelles textures et début d'animations

--- player.gd
+++ player.gd
@@ -1,5 +1,4 @@
-extends Node
-
+extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,18 +7,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var speed = 100
+	var speed = 3000
+	var input_direction = Input.get_vector("left","right","up","down")
 	
 	if Input.is_key_pressed(KEY_SHIFT):
-		speed = 300
-
-	if Input.is_key_pressed(KEY_Z):
-		$".".position.y -= speed * delta
-	if Input.is_key_pressed(KEY_S):
-		$".".position.y += speed * delta
-	if Input.is_key_pressed(KEY_Q):
-		$".".position.x -= speed * delta
-	if Input.is_key_pressed(KEY_D):
-		$".".position.x += speed * delta
+		speed = 9000
+	
+	velocity = input_direction * speed * delta
+	move_and_slide()
 		
 	pass
