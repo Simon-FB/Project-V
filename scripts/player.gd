b805ebdc5eb69d77b75ed32818b62c9fde7f7da8
Simon FALCONNET BANEYX <simon.falconnet-baneyx@etu.u-pec.fr>
2026-03-07 16:05:26 +0100

    walk et idle anim fini

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,7 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.play("idle_down_right")
-	pass # Replace with function body.
-
+	$Player_Anim.play("idle_down_right")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,56 +19,57 @@ func _process(delta: float) -> void:
 	
 	
 	# SPRITE CHANGE
-	var last_direction
-	print(input_direction)
+	Player.get_property_list()
+
 	#WALK
 	match input_direction[0]:
 		1.0: 
-			$AnimatedSprite2D.play("walk_right")
-			last_direction = "right"
+			$Player_Anim.play("walk_right")
+			Player.last_direction = "right"
 		-1.0:
-			$AnimatedSprite2D.play("walk_left")
-			last_direction = "left"
+			$Player_Anim.play("walk_left")
+			Player.last_direction = "left"
 		_:
 			match input_direction[1]:
 				1.0:
-					$AnimatedSprite2D.play("walk_down")
-					last_direction = "down"
+					$Player_Anim.play("walk_down")
+					Player.last_direction = "down"
+					
 				-1.0:
-					$AnimatedSprite2D.play("walk_up")
-					last_direction = "up"
+					$Player_Anim.play("walk_up")
+					Player.last_direction = "up"
 				_: 
 					if input_direction[0] > 0:
 						if input_direction[1] > 0:
-							$AnimatedSprite2D.play("walk_down_right")
-							last_direction = "down_right"
+							$Player_Anim.play("walk_down_right")
+							Player.last_direction = "down_right"
 						if input_direction[1] < 0:
-							$AnimatedSprite2D.play("walk_up_right")
-							last_direction = "up_right"
+							$Player_Anim.play("walk_up_right")
+							Player.last_direction = "up_right"
 					if input_direction[0] < 0:
 						if input_direction[1] > 0:
-							$AnimatedSprite2D.play("walk_down_left")
-							last_direction = "down_left"
+							$Player_Anim.play("walk_down_left")
+							Player.last_direction = "down_left"
 						if input_direction[1] < 0:
-							$AnimatedSprite2D.play("walk_up_left")
-							last_direction = "up_left"
+							$Player_Anim.play("walk_up_left")
+							Player.last_direction = "up_left"
 	#IDLE
 					if input_direction[0] == 0.0 and input_direction[1] == 0.0:
-						match last_direction:
+						match Player.last_direction:
 							"right":
-								$AnimatedSprite2D.play("idle_right")
+								$Player_Anim.play("idle_right")
 							"left":
-								$AnimatedSprite2D.play("idle_left")
+								$Player_Anim.play("idle_left")
 							"up":
-								$AnimatedSprite2D.play("idle_up")
+								$Player_Anim.play("idle_up")
 							"down":
-								$AnimatedSprite2D.play("idle_down")
+								$Player_Anim.play("idle_down")
 							"up_right":
-								$AnimatedSprite2D.play("idle_up_right")
+								$Player_Anim.play("idle_up_right")
 							"up_left":
-								$AnimatedSprite2D.play("idle_up_left")
+								$Player_Anim.play("idle_up_left")
 							"down_right":
-								$AnimatedSprite2D.play("idle_down_right")
+								$Player_Anim.play("idle_down_right")
 							"down_left":
-								$AnimatedSprite2D.play("idle_down_left")
+								$Player_Anim.play("idle_down_left")
 								
